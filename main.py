a9e93bdd5e1d0dba3f5652fcb8054fa14c570dae
ghostpall <piet.stam@gmail.com>
2026-04-25 13:34:47 +0200

    B: first player created

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import pygame
 import constants
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init() 
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0    
+    player_1 = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    
     while True:
         log_state()
         for event in pygame.event.get():
             pass
         screen.fill("black")
+        player_1.draw(screen)
         pygame.display.flip()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:
                 return
+        clock.tick(60)
+        dt = clock.tick(60)/1000
 
 
+        
 
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
